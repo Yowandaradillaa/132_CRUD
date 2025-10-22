ebef11bd90b7c68986ad073e5a0d6b9f41eefffc
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-10-22 11:01:04 +0700

    menambahkan koneksi mysql dan validasi connect

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
+const db = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  port: '3306',
+  password: '12345',
+  database: 'mahasiswa'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database:', err);
+        return;
+    }
+    console.log('Connected to database');
+});
+
 
 
 
