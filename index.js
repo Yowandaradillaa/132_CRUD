699c1637fba3460568f125f448f322fa909333d8
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-10-22 11:00:19 +0700

    menambahkan setup basic Express server with JSON and URL-encoded middleware

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+let mysql = require('mysql2');
+const app = express();
+const port = 3001;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+
+
